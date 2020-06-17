ecea96fe8ba772bfde7f96d8d5ed9af5224a3ee6
Rommel Rico <me@rommelrico.com>
2020-06-17 07:12:41 -0700

    :sparkles: App: Testing Yelp Secrets

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ENV from "./env";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text>Yelp Client ID: {ENV().yelpClientID}</Text>
+      <Text>Yelp API Key: {ENV().yelpAPIKey}</Text>
     </View>
   );
 }
