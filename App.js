b29a27cf874d15592e4b601c6d272c914d5db8c7
Rommel Rico <me@rommelrico.com>
2020-06-18 07:01:55 -0700

    :sparkles: App: Assembling a Navigator

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import ENV from "./env";
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Yelp Client ID: {ENV().yelpClientID}</Text>
-      <Text>Yelp API Key: {ENV().yelpAPIKey}</Text>
-    </View>
-  );
-}
+import SearchScreen from "./src/screens/SearchScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+  Search: SearchScreen
   },
-});
+  {
+    initialRouteName: "Search",
+    defaultNavigationOptions: {
+      title: "Business Search"
+    }
+  }
+);
+
+export default createAppContainer(navigator);
