8983cf0132096ecdcd437e07722bbf64c6aff071
Rommel Rico <me@rommelrico.com>
2020-06-19 06:17:01 -0700

    :sparkles: App: Adding state for results

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
+
 import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
 
 const SearchScreen = () => {
   const [searchTerm, setSearchTerm] =  useState("");
-
+  const [yelpResults, setYelpResults] = useState([]);
+  
   const onSearchTermChange = (text) => {
     setSearchTerm(text);
   };
@@ -20,7 +23,8 @@ const SearchScreen = () => {
         onSearchTermChange={onSearchTermChange}
         onSearchTermSubmit={onSearchTermSubmit}
       />
-      <Text>Search Term: {searchTerm}</Text>
+      <Text>Search Screen</Text>
+      <Text>We have found {yelpResults.length} results</Text>
     </View>
   );
 };
