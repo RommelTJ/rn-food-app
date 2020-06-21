783a0e971addd6358f0b520fe62525f08525e30d
Rommel Rico <me@rommelrico.com>
2020-06-21 11:51:52 -0700

    :tada: Nav: ResultsDetail

---
+++ src/components/ResultsDetail.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Image, StyleSheet, Text } from 'react-native';
+
+const ResultsDetail = (props) => {
+  return (
+    <View>
+      <Text>Results Detail</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ResultsDetail;
